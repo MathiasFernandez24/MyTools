d089d467ad0eee55de37b257f601847a8825779c
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-03-19 10:04:21 -0300

    card giratorio desarrollo

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,10 +5,20 @@ import { DB } from '../../dataBase/DB'
 const Card = () => {
 
     const i = DB[0]
+    const i2 = DB[1]
     return (
-        <div className='card-container'>
-            <div>{i.name}</div>
-            <img src={i.image} alt="" />
+        <div href={i.url} className='card-container'>
+            <a href={i.url} target='_blank'>
+                <div className='front card-container'>
+                    {i.name}
+                    <img className='card-image' src={i.image} alt="Loading.." />
+                </div>
+                <div className='back'>
+                    {i2.name}
+                    <img className='card-image' src={i2.image} alt="Loading.." />
+                </div>
+
+            </a>
         </div>
     )
 }
