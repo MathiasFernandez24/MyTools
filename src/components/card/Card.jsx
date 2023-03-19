6d9d41516cb64dcc11dedbb6b5b7d2eee6d9e907
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-03-19 14:06:16 -0300

    animacion card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Card.css'
 import { DB } from '../../dataBase/DB'
+import 'animate.css';
 
 const Card = ({ card }) => {
 
     const i = DB[0]
     return (
-        <div href={card.url} className='card-container'>
+        <div href={card.url} className='card-container animate__animated animate__fadeIn animate__slower'>
             <a href={card.url} target='_blank'>
                 <div className='front'>
                     <img className='card-image' src={card.image} alt="Loading.." />
