748fdd75a820015fbc47c54e7e32990cbe068478
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-03-19 16:20:38 -0300

    cambios en slider

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,7 @@ import flechaIzquierda from './imagenes/flecha-izquierda.png'
 import flechaDerecha from './imagenes/flecha-derecha.png'
 
 
+
 function Home() {
     return (
         <div className="container-home">
@@ -14,9 +15,7 @@ function Home() {
             <div id='contenedor-carrusel'>
                 <div className='item-carrusel' id='carrusel1'>
                     <div className='tarjeta-carrusel' id='tarjeta1'>
-                        
                             Lento, es la manera mas rápida de llegar a donde quieres estar.
-                        
                     </div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel3">
@@ -28,7 +27,9 @@ function Home() {
                     </div>
                 </div>
                 <div className='item-carrusel' id='carrusel2'>
-                    <div className='tarjeta-carrusel' id='tarjeta2'>B</div>
+                    <div className='tarjeta-carrusel' id='tarjeta2'>
+                        No renuncies, ni te rindas, todo lo grande toma tiempo.
+                    </div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel1">
                             <img src={flechaIzquierda} alt="" />
@@ -39,7 +40,9 @@ function Home() {
                     </div>
                 </div>
                 <div className='item-carrusel' id='carrusel3'>
-                    <div className='tarjeta-carrusel' id='tarjeta3'>C</div>
+                    <div className='tarjeta-carrusel' id='tarjeta3'>
+                        Los pasitos pequeños también te llevan a metas muy grandes.
+                    </div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel2">
                             <img src={flechaIzquierda} alt="" />
