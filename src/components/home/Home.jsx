9038cd8244c5ef85ad8b26dd19a110a950279cac
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-03-19 14:06:05 -0300

    cambios en carrusel, texto centrado

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,8 @@
 import './Home.css';
 import Cards from '../cards/Cards'
-// import Card from '../card/Card'
+import { IconArrowBigLeftFilled } from '@tabler/icons-react';
+import flechaIzquierda from './imagenes/flecha-izquierda.png'
+import flechaDerecha from './imagenes/flecha-derecha.png'
 
 
 function Home() {
@@ -11,13 +13,17 @@ function Home() {
             </div>
             <div id='contenedor-carrusel'>
                 <div className='item-carrusel' id='carrusel1'>
-                    <div className='tarjeta-carrusel' id='tarjeta1'>A</div>
+                    <div className='tarjeta-carrusel' id='tarjeta1'>
+                        
+                            Lento, es la manera mas rápida de llegar a donde quieres estar.
+                        
+                    </div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel3">
-                            <i>I</i>
+                            <img src={flechaIzquierda} alt="" />
                         </a>
                         <a href="#carrusel2">
-                            <i>D</i>
+                            <img src={flechaDerecha} alt="" />
                         </a>
                     </div>
                 </div>
@@ -25,10 +31,10 @@ function Home() {
                     <div className='tarjeta-carrusel' id='tarjeta2'>B</div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel1">
-                            <i>I</i>
+                            <img src={flechaIzquierda} alt="" />
                         </a>
                         <a href="#carrusel3">
-                            <i>D</i>
+                            <img src={flechaDerecha} alt="" />
                         </a>
                     </div>
                 </div>
@@ -36,10 +42,10 @@ function Home() {
                     <div className='tarjeta-carrusel' id='tarjeta3'>C</div>
                     <div className='flechas-carrusel'>
                         <a href="#carrusel2">
-                            <i>I</i>
+                            <img src={flechaIzquierda} alt="" />
                         </a>
                         <a href="#carrusel1">
-                            <i>D</i>
+                            <img src={flechaDerecha} alt="" />
                         </a>
                     </div>
                 </div>
