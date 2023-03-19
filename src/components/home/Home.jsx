f01a9453895af8800dcfce52bceec353bbec45cb
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-03-19 12:45:53 -0300

    cambios en carrusel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,25 +10,37 @@ function Home() {
                 Nuestras Herramientas
             </div>
             <div id='contenedor-carrusel'>
-                <div className='item-carrusel'>
+                <div className='item-carrusel' id='carrusel1'>
                     <div className='tarjeta-carrusel' id='tarjeta1'>A</div>
                     <div className='flechas-carrusel'>
-                        <i>I</i>
-                        <i>D</i>
+                        <a href="#carrusel3">
+                            <i>I</i>
+                        </a>
+                        <a href="#carrusel2">
+                            <i>D</i>
+                        </a>
                     </div>
                 </div>
-                <div className='item-carrusel'>
+                <div className='item-carrusel' id='carrusel2'>
                     <div className='tarjeta-carrusel' id='tarjeta2'>B</div>
                     <div className='flechas-carrusel'>
-                        <i>I</i>
-                        <i>D</i>
+                        <a href="#carrusel1">
+                            <i>I</i>
+                        </a>
+                        <a href="#carrusel3">
+                            <i>D</i>
+                        </a>
                     </div>
                 </div>
-                <div className='item-carrusel'>
+                <div className='item-carrusel' id='carrusel3'>
                     <div className='tarjeta-carrusel' id='tarjeta3'>C</div>
                     <div className='flechas-carrusel'>
-                        <i>I</i>
-                        <i>D</i>
+                        <a href="#carrusel2">
+                            <i>I</i>
+                        </a>
+                        <a href="#carrusel1">
+                            <i>D</i>
+                        </a>
                     </div>
                 </div>
             </div>
