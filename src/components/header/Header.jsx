1a1c7985cb3a3f28723fdd4088772d1912d56358
Mathias Fernandez <mathias_fernandez_24@hotmail.com>
2023-03-26 19:14:09 -0300

    links linkedin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import { IconBrandLinkedin } from '@tabler/icons-react';
 const header = () => {
     return (
         <div className='barra-navegacion animate__animated animate__bounceInDown'>
-            <a href='https://www.linkedin.com/in/mathias-nicolas-fernandez-figueroa-98450482/' target={'_blank'} className='linkedin-click'>
+            <a href='https://www.linkedin.com/in/maira-julian-768a001b0/' target={'_blank'} className='linkedin-click'>
                 Maira
                 <IconBrandLinkedin />
             </a>
